78a2e4c327a242fffd860092a527f67077e79fdb
Jordan Adams <hi@jordanadams.co.uk>
2016-05-03 15:11:30 +0100

    Adds minify step to js builds

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -6,11 +6,16 @@ const plumber = require('gulp-plumber');
 const browsersync = require('browser-sync');
 const config = require('../config');
 
-module.exports = () => {
-  const bundler = browserify(config.js.entries, {
-    debug: true
-  });
+const bundler = browserify({debug: true});
+bundler.add(config.js.entries);
+bundler.transform('babelify', {
+  presets: ['es2015', 'stage-2', 'react']
+});
+bundler.plugin('minifyify', {
+  output: './dist/app.js.map'
+});
 
+module.exports = () => {
   bundler.bundle()
     .pipe(plumber())
     .pipe(source('app.min.js'))
